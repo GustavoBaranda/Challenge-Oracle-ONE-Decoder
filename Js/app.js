be6a60e2e38b2261815d7c5d727301b584c460cd
Gustavo Baranda <baranda.gustavo@gmail.com>
2023-04-12 20:26:33 -0300

    encryption logic

--- Js/app.js
+++ Js/app.js
@@ -3,6 +3,21 @@ const btnEncoder = document.getElementById('btn--encoder');
 const btnDecoder = document.getElementById('btn--decoder');
 const message = document.getElementById('message');
 const btnCopy = document.getElementById('btn--copy');
+let vowels = {
+    'a' : 'ai',
+    'e' : 'enter',
+    'i' : 'imes',
+    'o' : 'ober',
+    'u' : 'ufat'
+}
+
+let vowelsEncoder = {
+    'ai'    : 'a',
+    'enter' : 'e',
+    'imes'  : 'i',
+    'ober'  : 'o',
+    'ufat'  : 'u'
+}
 
 
 const validation = () => {
@@ -13,12 +28,31 @@ const validation = () => {
     } else {
         document.querySelector('.decoder__content--messageDefauld').style.display = 'none';
         document.querySelector('.decoder__content--decoderMessage').style.display = 'block';
+    }    
+}
+
+const encoder = () => {
+    const encryptedText = (input) => {
+        let newInput = '';
+        newInput = input.replace(/[aeiou]/g, i => vowels[i]);
+        return newInput;
     }
-    console.log(inputTexT.value)
+    message.value = encryptedText(inputTexT.value);
+}
+
+const decoder = () => {
+    const decryptedText = (input) => {
+        let newInput = '';
+        newInput = input.replace(/ai|enter|imes|ober|ufat/g, i => vowelsEncoder[i]);
+        return newInput;
+    }
+    message.value = decryptedText(inputTexT.value);
 }
 
 btnEncoder.addEventListener('click', validation);
-
+btnEncoder.addEventListener('click', encoder);
+btnDecoder.addEventListener('click', validation);
+btnDecoder.addEventListener('click', decoder);
 
 
 
